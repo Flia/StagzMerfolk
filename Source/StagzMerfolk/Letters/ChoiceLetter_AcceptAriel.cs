8515b274947053be6e2970eb484b4f813c97dc8b
Flia <miracleshift@gmail.com>
2025-07-19 00:28:57 +0200

    First hacky build for 1.6
    Re-built for 1.6. I think. Hopefully I did okay.
    Changed "createsNeed" to "enablesNeeds" for aquatic gene. That shoots down one red error, though the need doesn't work in the game yet.

--- Source/StagzMerfolk/Letters/ChoiceLetter_AcceptAriel.cs
+++ Source/StagzMerfolk/Letters/ChoiceLetter_AcceptAriel.cs
@@ -13,14 +13,13 @@ public class ChoiceLetter_AcceptAriel : ChoiceLetter_AcceptCharmedJoiner
             {
                 GenExplosion.DoExplosion(asker.Position, asker.Map, 4.9f, DamageDefOf.Extinguish, null, -1, -1f, 
                     SoundDefOf.Explosion_FirefoamPopper, null, null, null, ThingDefOf.Filth_FireFoam, 1f, 1, null, 
-                    true, null, 0f, 1, 0f, false, null, null, null, 
-                    true, 1f, 0f, true, null, 1f);
+                    0f, 0,true);
                 
                 asker.Kill(null);
                 CompRottable comp;
                 if (asker.ParentHolder is Corpse c && (comp = c.GetComp<CompRottable>()) != null)
                 {
-                    comp.RotProgress = (float)comp.PropsRot.TicksToDessicated;
+                    comp.RotProgress = comp.PropsRot.TicksToDessicated;
                 }
                 Find.LetterStack.RemoveLetter(this);
             },
