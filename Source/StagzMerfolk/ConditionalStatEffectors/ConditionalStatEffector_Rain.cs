744aee642b457ea995f0e525a7764eaea4b1faf5
Flia <miracleshift@gmail.com>
2026-02-15 11:32:53 +0100

    WIP on aquatic need & related cs SQUASH
    - ConditionalStatAffectors: Biotech checks looked very unnecessary + some format
    - StagzUtils: SoakingWet check should not have worked in the first place. IsWet removed because whenever it was called it was either IsRain || OnWater check, or in the aquatic need itself, for which I want a more robust system
    - Need_Aquatic: Most of it is just an effort to make it look more like vanilla needs are coded, which makes it easier to read & copypaste solutions. Also, a first attempt at giving Standalone Hot Spring come compatibility.
    Future plan: staring some more at Need_Joy, seeing if I can implement an abstract system that allows to easily add jobs which give hydration. That could allow merfolk to use SHS or DBH or <insert_mod_here> features to hydrate themselves.

--- Source/StagzMerfolk/ConditionalStatEffectors/ConditionalStatEffector_Rain.cs
+++ Source/StagzMerfolk/ConditionalStatEffectors/ConditionalStatEffector_Rain.cs
@@ -7,13 +7,7 @@ public class ConditionalStatEffector_Rain : ConditionalStatAffecter
 {
     public override bool Applies(StatRequest req)
     {
-        //Check if biotech is active
-        if (!ModsConfig.BiotechActive)
-        {
-            return false;
-        }
-
-        //Check if request is valid (request is for a pawn and pawn is on a map
+        //Check if request is valid (request is for a pawn and pawn is on a map)
         var pawn = req.Thing as Pawn;
         if (!req.HasThing || pawn?.Map == null) return false;
 
@@ -21,8 +15,5 @@ public class ConditionalStatEffector_Rain : ConditionalStatAffecter
         return pawn.InRain();
     }
 
-    public override string Label
-    {
-        get { return "StagzMerfolk_ConditionalStatEffector_Rain".Translate(); }
-    }
+    public override string Label => "StagzMerfolk_ConditionalStatEffector_Rain".Translate();
 }
